23ff39036d6367c5ae217e1ca57fc4b703e30d73
sergio-nunez <sergio.nunez@laposte.net>
2023-09-05 21:32:02 +0200

    Variable $mainMenu outsourced from header.php file

---
+++ lib/menu.php
@@ -0,0 +1,6 @@
+<?php
+$mainMmenu = [
+    'index.php'      => ['menu_title' => 'Accueil', 'head_title' => 'Accueil TechTrendz', 'meta_description' => 'TechTrendz, l\'actu tech !'],
+    'a_propos.php'   => ['menu_title' => 'A propos', 'head_title' => 'A propos de TechTrendz', 'meta_description' => 'L\'histoire du site TeckTrendz'],
+    'actualites.php' => ['menu_title' => 'Actualités Tech !', 'head_title' => 'Actualités TechTrendz', 'meta_description' => 'Découvrez toutes nos actualitech !']
+];
--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,8 @@
 <?php
-$mainMmenu = [
-    'index.php'      => ['menu_title' => 'Accueil', 'head_title' => 'Accueil TechTrendz', 'meta_description' => 'TechTrendz, l\'actu tech !'],
-    'a_propos.php'   => ['menu_title' => 'A propos', 'head_title' => 'A propos de TechTrendz', 'meta_description' => 'L\'histoire du site TeckTrendz'],
-    'actualites.php' => ['menu_title' => 'Actualités Tech !', 'head_title' => 'Actualités TechTrendz', 'meta_description' => 'Découvrez toutes nos actualitech !']
-];
 
-$currentPage = basename($_SERVER['SCRIPT_NAME']);
+require_once __DIR__ . ('../../lib/menu.php');
+
+$currentPage = basename($_SERVER['SCRIPT_NAME']); // la fonction basename() affiche la ressource sans le chemin (path)
 //echo $currentPage;
 
 ?>
